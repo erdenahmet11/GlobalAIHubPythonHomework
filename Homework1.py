18851d02374cb133ff88492b59a425ebc6123eeb
erdenahmet11 <60483008+erdenahmet11@users.noreply.github.com>
2020-12-22 15:24:35 +0300

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,9 @@
+value_1 = int(input("Enter the first value: "))
+value_2 = float(input("Enter the second value: "))
+value_3 = input("Enter the third value: ")
+value_4 = input("Enter the fourth value: ")
+value_5 = input("Enter the fifth value: ")
+
+print("The first value = {}, the second value = {}, The third value = {}, The fourth value = {}, The fifth value = {}".format(value_1,value_2,value_3,value_4,value_5))
+print("Type of the first value = {}, Type of the second value = {}, Type of the third value = {}, Type of the fourth value = {}, Type of the fifth value = {}"
+      .format(type(value_1),type(value_2),type(value_3),type(value_4),type(value_5)))
